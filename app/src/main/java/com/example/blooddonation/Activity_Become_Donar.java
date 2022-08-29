c641d6d0b4ec2df8effbae21c191b6cfb5189a1d
Khalekuzzaman <khalekuzzamancse@gmail.com>
2022-08-29 21:24:13 +0600

    Updated

--- app/src/main/java/com/example/blooddonation/Activity_Become_Donar.java
+++ app/src/main/java/com/example/blooddonation/Activity_Become_Donar.java
@@ -3,6 +3,14 @@ package com.example.blooddonation;
 import androidx.appcompat.app.AppCompatActivity;
 
 import android.os.Bundle;
+import android.util.Log;
+import android.view.View;
+import android.widget.AdapterView;
+import android.widget.ArrayAdapter;
+import android.widget.AutoCompleteTextView;
+
+import java.util.ArrayList;
+import java.util.List;
 
 public class Activity_Become_Donar extends AppCompatActivity {
 
@@ -10,5 +18,57 @@ public class Activity_Become_Donar extends AppCompatActivity {
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_become_donar_scrollable);
+            setGender();
+            setBloodGroup();
+
+
+
+
     }
+    private void setGender()
+    {
+        List<String> genderList=new ArrayList<>();
+        genderList.add("Male");
+        genderList.add("Female");
+        genderList.add("Other");
+//
+        ArrayAdapter<String> adapter=new ArrayAdapter<>(this,R.layout.layout_drop_down_menu_single_item,genderList);
+        AutoCompleteTextView gender=findViewById(R.id.Activity_BecomeDonar_TextInputLayout_AutoCompleteTextView_Gender);
+        gender.setAdapter(adapter);
+
+        gender.setOnItemClickListener(new AdapterView.OnItemClickListener() {
+            @Override
+            public void onItemClick(AdapterView<?> parent, View view, int position, long id) {
+                String s=  parent.getItemAtPosition(position).toString();
+                Log.i("Clickeed",s);
+
+            }
+        });
+    }
+    private void setBloodGroup()
+    {
+        List<String> BloodGroupList=new ArrayList<>();
+        BloodGroupList.add("A+");
+        BloodGroupList.add("A-");
+        BloodGroupList.add("B+");
+        BloodGroupList.add("B-");
+        BloodGroupList.add("O+");
+        BloodGroupList.add("O-");
+        BloodGroupList.add("AB+");
+        BloodGroupList.add("AB-");
+//
+        ArrayAdapter<String> adapter=new ArrayAdapter<>(this,R.layout.layout_drop_down_menu_single_item,BloodGroupList);
+        AutoCompleteTextView bloodGroup=findViewById(R.id.Activity_BecomeDonar_TextInputLayout_AutoCompleteTextView_BloodGroup);
+        bloodGroup.setAdapter(adapter);
+
+        bloodGroup.setOnItemClickListener(new AdapterView.OnItemClickListener() {
+            @Override
+            public void onItemClick(AdapterView<?> parent, View view, int position, long id) {
+                String s=  parent.getItemAtPosition(position).toString();
+                Log.i("Clickeed",s);
+
+            }
+        });
+    }
+
 }
--- app/src/main/java/com/example/blooddonation/Activity_SearchBlood.java
+++ app/src/main/java/com/example/blooddonation/Activity_SearchBlood.java
@@ -3,6 +3,14 @@ package com.example.blooddonation;
 import androidx.appcompat.app.AppCompatActivity;
 
 import android.os.Bundle;
+import android.util.Log;
+import android.view.View;
+import android.widget.AdapterView;
+import android.widget.ArrayAdapter;
+import android.widget.AutoCompleteTextView;
+
+import java.util.ArrayList;
+import java.util.List;
 
 public class Activity_SearchBlood extends AppCompatActivity {
 
@@ -10,5 +18,31 @@ public class Activity_SearchBlood extends AppCompatActivity {
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_search_blood);
+        setBloodGroup();
+    }
+    private void setBloodGroup()
+    {
+        List<String> BloodGroupList=new ArrayList<>();
+        BloodGroupList.add("A+");
+        BloodGroupList.add("A-");
+        BloodGroupList.add("B+");
+        BloodGroupList.add("B-");
+        BloodGroupList.add("O+");
+        BloodGroupList.add("O-");
+        BloodGroupList.add("AB+");
+        BloodGroupList.add("AB-");
+//
+        ArrayAdapter<String> adapter=new ArrayAdapter<>(this,R.layout.layout_drop_down_menu_single_item,BloodGroupList);
+        AutoCompleteTextView bloodGroup=findViewById(R.id.Activity_SearchBlood_TextInputLayout_AutoCompleteTextView_BloodGroup);
+        bloodGroup.setAdapter(adapter);
+
+        bloodGroup.setOnItemClickListener(new AdapterView.OnItemClickListener() {
+            @Override
+            public void onItemClick(AdapterView<?> parent, View view, int position, long id) {
+                String s=  parent.getItemAtPosition(position).toString();
+                Log.i("Clickeed",s);
+
+            }
+        });
     }
 }
