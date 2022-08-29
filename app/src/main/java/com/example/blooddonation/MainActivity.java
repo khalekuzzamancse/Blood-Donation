41467c8266c0df599c17610abc2def1dc7d031c6
Khalekuzzaman <khalekuzzamancse@gmail.com>
2022-08-29 20:23:21 +0600

    Updated

--- app/src/main/java/com/example/blooddonation/MainActivity.java
+++ app/src/main/java/com/example/blooddonation/MainActivity.java
@@ -25,7 +25,7 @@ public class MainActivity extends AppCompatActivity {
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
-        setContentView(R.layout.activity_become_donar);
+        setContentView(R.layout.activity_main);
 //        Read();
 //        Write();
 
