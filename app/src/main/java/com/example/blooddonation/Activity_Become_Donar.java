fa5c21272b8cd3421268c5950b3e164b59889308
Khalekuzzaman <khalekuzzamancse@gmail.com>
2022-08-29 18:39:54 +0600

    Updated

---
+++ app/src/main/java/com/example/blooddonation/Activity_Become_Donar.java
@@ -0,0 +1,14 @@
+package com.example.blooddonation;
+
+import androidx.appcompat.app.AppCompatActivity;
+
+import android.os.Bundle;
+
+public class Activity_Become_Donar extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_become_donar);
+    }
+}
