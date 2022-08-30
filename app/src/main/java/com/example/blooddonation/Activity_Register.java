3f1df72ae850503850c05a757e1a84e135f90ec9
Khalekuzzaman <khalekuzzamancse@gmail.com>
2022-08-31 00:15:55 +0600

    Updated

--- app/src/main/java/com/example/blooddonation/Activity_Register.java
+++ app/src/main/java/com/example/blooddonation/Activity_Register.java
@@ -30,62 +30,62 @@ public class Activity_Register extends AppCompatActivity {
         Objects.requireNonNull(getSupportActionBar()).setDisplayHomeAsUpEnabled(true);
         getSupportActionBar().setTitle("Register");
 
-//        Button submit=findViewById(R.id.Activity_Register_Button_Submit);
-//        submit.setOnClickListener(view -> {
-//          ;
-//        });
+        Button submit=findViewById(R.id.Activity_Register_Button_Submit);
+        submit.setOnClickListener(view -> {
+          SetUserInfo();
+        });
 
     }
-//    @Override
-//    public boolean onCreateOptionsMenu(Menu menu) {
-//        MenuInflater inflater = getMenuInflater();
-//        inflater.inflate(R.menu.menu_item_for_non_home_activity_toolbar, menu);
-//        return true;
-//    }
-//    @Override
-//    public boolean onOptionsItemSelected(MenuItem item) {
-//        int id=item.getItemId();
-//        if(id==R.id.NonHomeActivity_MenuItem_SearchBlood)
-//        {
-//            Intent intent=new Intent(this,Activity_SearchBlood.class);
-//            startActivity(intent);
-//        }
-//        return super.onOptionsItemSelected(item);
-//    }
-//
-//    private void Register(String email,String password)
-//    {
-//        FirebaseAuth mAuth=FirebaseAuth.getInstance();
-//        mAuth.createUserWithEmailAndPassword(email,password)
-//                .addOnCompleteListener((Task<AuthResult> task)->{
-//                    if(!task.isSuccessful())
-//                    {
-//                        Log.i("Logined","Next,Inshallah");
-//                    }
-//                    else
-//                    {
-//                        Log.i("Logined","Alhadulliah");
-//                        Intent intent=new Intent(this,MainActivity.class);
-//                        startActivity(intent);
-//                    }
-//                });
-//    }
-//    private void SetUserInfo()
-//    {
-//        EditText Name=findViewById(R.id.Activity_Register_TextInputLayout_EditText_Name);
-//        String name=Name.getText().toString().trim();
-//        EditText Email=findViewById(R.id.Activity_Register_TextInputLayout_EditText_Email);
-//        String email=Email.getText().toString().trim();
-//        EditText PhoneNumber=findViewById(R.id.Activity_Register_TextInputLayout_EditText_PhoneNumber);
-//        String phoneNumber=PhoneNumber.getText().toString().trim();
-//        EditText UserName=findViewById(R.id.Activity_Register_TextInputLayout_EditText_UserName);
-//        String userName=UserName.getText().toString().trim();
-//        EditText PassWord=findViewById(R.id.Activity_Register_TextInputLayout_EditText_Password);
-//        String password=PassWord.getText().toString().trim();
-//        EditText ConfirmPassWord=findViewById(R.id.Activity_Register_TextInputLayout_EditText_ConfirmPassword);
-//        String confirmPassword=ConfirmPassWord.getText().toString().trim();
-//        Log.i("Alhamdulillah",name+"\n"+email+"\n"+phoneNumber+"\n"+userName+"\n"+password+"\n"+confirmPassword);
-//
-//    }
+    @Override
+    public boolean onCreateOptionsMenu(Menu menu) {
+        MenuInflater inflater = getMenuInflater();
+        inflater.inflate(R.menu.menu_item_for_non_home_activity_toolbar, menu);
+        return true;
+    }
+    @Override
+    public boolean onOptionsItemSelected(MenuItem item) {
+        int id=item.getItemId();
+        if(id==R.id.NonHomeActivity_MenuItem_SearchBlood)
+        {
+            Intent intent=new Intent(this,Activity_SearchBlood.class);
+            startActivity(intent);
+        }
+        return super.onOptionsItemSelected(item);
+    }
+
+    private void Register(String email,String password)
+    {
+        FirebaseAuth mAuth=FirebaseAuth.getInstance();
+        mAuth.createUserWithEmailAndPassword(email,password)
+                .addOnCompleteListener((Task<AuthResult> task)->{
+                    if(!task.isSuccessful())
+                    {
+                        Log.i("Logined","Next,Inshallah");
+                    }
+                    else
+                    {
+                        Log.i("Logined","Alhadulliah");
+                        Intent intent=new Intent(this,MainActivity.class);
+                        startActivity(intent);
+                    }
+                });
+    }
+    private void SetUserInfo()
+    {
+        EditText Name=findViewById(R.id.Activity_Register_TextInputLayout_EditText_Name);
+        String name=Name.getText().toString().trim();
+        EditText Email=findViewById(R.id.Activity_Register_TextInputLayout_EditText_Email);
+        String email=Email.getText().toString().trim();
+        EditText PhoneNumber=findViewById(R.id.Activity_Register_TextInputLayout_EditText_PhoneNumber);
+        String phoneNumber=PhoneNumber.getText().toString().trim();
+        EditText UserName=findViewById(R.id.Activity_Register_TextInputLayout_EditText_UserName);
+        String userName=UserName.getText().toString().trim();
+        EditText PassWord=findViewById(R.id.Activity_Register_TextInputLayout_EditText_Password);
+        String password=PassWord.getText().toString().trim();
+        EditText ConfirmPassWord=findViewById(R.id.Activity_Register_TextInputLayout_EditText_ConfirmPassword);
+        String confirmPassword=ConfirmPassWord.getText().toString().trim();
+        Log.i("Alhamdulillah\n",name+"\n"+email+"\n"+phoneNumber+"\n"+userName+"\n"+password+"\n"+confirmPassword);
+
+    }
 
 }
