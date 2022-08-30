28fddd755f5a28bf4f9018c7d76963e180ff2c33
Khalekuzzaman <khalekuzzamancse@gmail.com>
2022-08-30 20:58:03 +0600

    Updated

--- app/src/main/java/com/example/blooddonation/Activity_AboutUs.java
+++ app/src/main/java/com/example/blooddonation/Activity_AboutUs.java
@@ -1,8 +1,16 @@
 package com.example.blooddonation;
 
+import androidx.annotation.NonNull;
+import androidx.appcompat.app.ActionBarDrawerToggle;
 import androidx.appcompat.app.AppCompatActivity;
+import androidx.appcompat.widget.Toolbar;
+import androidx.drawerlayout.widget.DrawerLayout;
 
+import android.content.Intent;
 import android.os.Bundle;
+import android.view.MenuItem;
+
+import com.google.android.material.navigation.NavigationView;
 
 public class Activity_AboutUs extends AppCompatActivity {
 
@@ -10,5 +18,6 @@ public class Activity_AboutUs extends AppCompatActivity {
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_about_us);
+
     }
 }
