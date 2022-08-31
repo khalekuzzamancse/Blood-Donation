e3d3b6d50fd4078beadc618f8c91ef3c21f0e5ed
Khalekuzzaman <khalekuzzamancse@gmail.com>
2022-08-31 15:57:25 +0600

    Updated

---
+++ app/src/main/java/com/example/blooddonation/DataTypeForRecyclerView_ActivitySearchResult.java
@@ -0,0 +1,12 @@
+package com.example.blooddonation;
+
+public class DataTypeForRecyclerView_ActivitySearchResult {
+    public String Name;
+    public String PhoneNumber;
+    public String Email;
+    public String BloodGroup;
+    public String District;
+    public String SubDistrict;
+
+
+}
---
+++ app/src/main/java/com/example/blooddonation/ViewHolderForRecyclerViewLoggedIn.java
@@ -0,0 +1,28 @@
+package com.example.blooddonation;
+
+import android.view.View;
+import android.widget.TextView;
+
+import androidx.annotation.NonNull;
+import androidx.recyclerview.widget.RecyclerView;
+
+public class ViewHolderForRecyclerViewLoggedIn extends RecyclerView.ViewHolder {
+        public TextView TextView_ViewHolder_Name;
+        public TextView TextView_ViewHolder_PhoneNumber;
+        public TextView TextView_ViewHolder_Email;
+        public TextView TextView_ViewHolder_BloodGroup;
+        public TextView TextView_ViewHolder_District;
+        public TextView TextView_ViewHolder_SubDistrict;
+        public  ViewHolderForRecyclerViewLoggedIn(@NonNull View itemView) {
+            super(itemView);
+
+            TextView_ViewHolder_Name=itemView.findViewById(R.id.Text_RecyclerViewLayout_SearchOnLoggedIn_Name);
+            TextView_ViewHolder_PhoneNumber=itemView.findViewById(R.id.Text_RecyclerViewLayout_SearchOnLoggedIn_PhoneNumber);
+            TextView_ViewHolder_Email=itemView.findViewById(R.id.Text_RecyclerViewLayout_SearchOnLoggedIn_Email);
+            TextView_ViewHolder_BloodGroup=itemView.findViewById(R.id.Text_RecyclerViewLayout_SearchOnLoggedIn_BloodGroup);
+            TextView_ViewHolder_District=itemView.findViewById(R.id.Text_RecyclerViewLayout_SearchOnLoggedIn_District);
+            TextView_ViewHolder_SubDistrict=itemView.findViewById(R.id.Text_RecyclerViewLayout_SearchOnLoggedIn_SubDistrict);
+        }
+
+
+}
