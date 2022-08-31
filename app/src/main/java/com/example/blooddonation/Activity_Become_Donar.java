3a75df8308d7954b5f51f52e275b98b0b40d3bb3
Khalekuzzaman <khalekuzzamancse@gmail.com>
2022-08-31 13:23:09 +0600

    Updated

--- app/src/main/java/com/example/blooddonation/Activity_Become_Donar.java
+++ app/src/main/java/com/example/blooddonation/Activity_Become_Donar.java
@@ -1,5 +1,8 @@
 package com.example.blooddonation;
 
+import static android.content.ContentValues.TAG;
+
+import androidx.annotation.NonNull;
 import androidx.appcompat.app.AppCompatActivity;
 import androidx.appcompat.widget.Toolbar;
 
@@ -14,11 +17,16 @@ import android.widget.AdapterView;
 import android.widget.ArrayAdapter;
 import android.widget.AutoCompleteTextView;
 import android.widget.Button;
+import android.widget.EditText;
 
+import com.google.android.gms.tasks.OnCompleteListener;
 import com.google.android.gms.tasks.Task;
 import com.google.firebase.auth.FirebaseAuth;
 import com.google.firebase.auth.FirebaseUser;
+import com.google.firebase.firestore.DocumentSnapshot;
 import com.google.firebase.firestore.FirebaseFirestore;
+import com.google.firebase.firestore.QueryDocumentSnapshot;
+import com.google.firebase.firestore.QuerySnapshot;
 import com.google.firebase.firestore.SetOptions;
 
 import java.util.ArrayList;
@@ -27,7 +35,6 @@ import java.util.List;
 
 public class Activity_Become_Donar extends AppCompatActivity {
 
-    private HashMap<String, Object> data;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
@@ -40,12 +47,10 @@ public class Activity_Become_Donar extends AppCompatActivity {
 
             setGender();
             setBloodGroup();
+            DistrictList();
         Button submit=findViewById(R.id.Activity_BecomeDonar_Button_Submit);
         submit.setOnClickListener(view -> {
-            HashMap<String,Object>Data=new HashMap<>();
-            Data.put("BloodGroup","A+");
-            addANewField(Data);
-
+            AddDonorInfo();
         });
 //this is comment
 
@@ -89,6 +94,7 @@ public class Activity_Become_Donar extends AppCompatActivity {
         });
     }
     private void setBloodGroup()
+
     {
         List<String> BloodGroupList=new ArrayList<>();
         BloodGroupList.add("A+");
@@ -113,6 +119,120 @@ public class Activity_Become_Donar extends AppCompatActivity {
             }
         });
     }
+    private void DistrictList()
+    {
+        List<String> districtList=new ArrayList<>();
+        FirebaseFirestore db = FirebaseFirestore.getInstance();
+        db.collection("DistrictList")
+                .get()
+                .addOnCompleteListener(new OnCompleteListener<QuerySnapshot>() {
+                    @Override
+                    public void onComplete(@NonNull Task<QuerySnapshot> task) {
+                        if (task.isSuccessful()) {
+                            for (QueryDocumentSnapshot document : task.getResult()) {
+                                Log.d("Hello", document.getId() + " => " + document.getData());
+                                // String name=(String)document.get("Name");
+                                String name=(String)document.getId();
+                                Log.i("District",name);
+                                districtList.add(name);
+                            }
+
+                            ArrayAdapter<String> adapter=new ArrayAdapter<>(Activity_Become_Donar.this,R.layout.layout_drop_down_menu_single_item,districtList);
+                            AutoCompleteTextView d=
+                                    findViewById(R.id.Activity_BecomeDonar_TextInputLayout_AutoCompleteTextView_District);
+                            d.setAdapter(adapter);
+
+                            d.setOnItemClickListener(new AdapterView.OnItemClickListener() {
+                                @Override
+                                public void onItemClick(AdapterView<?> parent, View view, int position, long id) {
+                                    String s=  parent.getItemAtPosition(position).toString();
+                                    Log.i("Clickeed",s);
+                                    setSubDistrict(s);
+                                }
+                            });
+                        }
+
+
+                        else {
+                            Log.w(TAG, "Error getting documents.", task.getException());
+                        }
+                    }
+                });
+    }
+    private void setSubDistrict(String s)
+    {
+
+        FirebaseFirestore db = FirebaseFirestore.getInstance();
+        db.collection("DistrictList")
+                .document(s)
+                .get()
+                .addOnCompleteListener(new OnCompleteListener<DocumentSnapshot>() {
+                    @Override
+                    public void onComplete(@NonNull Task<DocumentSnapshot> task) {
+                        if (task.isSuccessful()) {
+                            DocumentSnapshot document = task.getResult();
+                            if (document.exists()) {
+                                List<String> subDistrictList = (List<String>) document.get("SubDistrict");
+                                for (String it : subDistrictList) {
+                                    Log.d("Upozilla", it);
+                                }
+                                ArrayAdapter<String> adapter=new ArrayAdapter<>(Activity_Become_Donar.this,R.layout.layout_drop_down_menu_single_item,subDistrictList);
+                                AutoCompleteTextView d=
+                                        findViewById(R.id.Activity_BecomeDonar_TextInputLayout_AutoCompleteTextView_SubDistrict);
+                                d.setAdapter(adapter);
+
+                                d.setOnItemClickListener(new AdapterView.OnItemClickListener() {
+                                    @Override
+                                    public void onItemClick(AdapterView<?> parent, View view, int position, long id) {
+                                        String s=  parent.getItemAtPosition(position).toString();
+                                        Log.i("Clickeed",s);
+
+
+                                    }
+                                });
+
+
+
+                            } else {
+                                Log.d(TAG, "No such document");
+                            }
+                        } else {
+                            Log.d(TAG, "get failed with ", task.getException());
+                        }
+                    }
+                });
+    }
+
+    private void AddDonorInfo()
+    {
+        EditText age=findViewById(R.id.Activity_BecomeDonar_TextInputLayout_EditText_Age);
+        String Age=age.getText().toString().trim();
+        HashMap<String,Object>setAge=new HashMap<>();
+        setAge.put("Age",Age);
+        addANewField(setAge);
+        EditText gender=findViewById(R.id.Activity_BecomeDonar_TextInputLayout_AutoCompleteTextView_Gender);
+        String Gender=gender.getText().toString().trim();
+        HashMap<String,Object>setGender=new HashMap<>();
+        setGender.put("Gender",Gender);
+        addANewField(setGender);
+        EditText bloodGroup=findViewById(R.id.Activity_BecomeDonar_TextInputLayout_AutoCompleteTextView_BloodGroup);
+        String blood=bloodGroup.getText().toString().trim();
+        HashMap<String,Object>setBloodGroup=new HashMap<>();
+        setBloodGroup.put("BloodGroup",blood);
+        addANewField(setBloodGroup);
+        EditText Dis=findViewById(R.id.Activity_BecomeDonar_TextInputLayout_AutoCompleteTextView_District);
+        String dis=Dis.getText().toString().trim();
+        HashMap<String,Object>setDis=new HashMap<>();
+        setDis.put("District",dis);
+        addANewField(setDis);
+        EditText SubDis=findViewById(R.id.Activity_BecomeDonar_TextInputLayout_AutoCompleteTextView_SubDistrict);
+        String subDis=SubDis.getText().toString().trim();
+        HashMap<String,Object>setSubDis=new HashMap<>();
+        setSubDis.put("SubDistrict",subDis);
+        addANewField(setSubDis);
+        Log.i("Alhadlliah",Age+" "+Gender+" "+blood+" "+dis+" "+subDis);
+    }
+
     private void addANewField(HashMap<String,Object>Data)
     {
         FirebaseAuth mAuth; mAuth=FirebaseAuth.getInstance();
