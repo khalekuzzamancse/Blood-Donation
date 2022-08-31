165bd1f74cb9624686d414e11797093633c0ca14
Khalekuzzaman <khalekuzzamancse@gmail.com>
2022-09-01 00:12:39 +0600

    Updated

---
+++ app/src/main/java/com/example/blooddonation/CurrentUserInfo.java
@@ -0,0 +1,17 @@
+package com.example.blooddonation;
+
+public class CurrentUserInfo {
+
+    public  static String name;
+    public  static String userName;
+    public  static String email;
+    public  static String phone;
+    public  static String district;
+    public  static String subDistrict;
+    public  static String bloodGroup;
+    public  static String gender;
+    public  static String age;
+    public  static String isDonor;
+    public  static String isSingedIn;
+
+}
