0ee954b4c37f1c396c865bd2d7405b9860f59535
Khalekuzzaman <khalekuzzamancse@gmail.com>
2022-09-01 18:55:10 +0600

    Updated

--- app/src/main/java/com/example/blooddonation/Activity_UpdateProfile.java
+++ app/src/main/java/com/example/blooddonation/Activity_UpdateProfile.java
@@ -1,14 +1,40 @@
 package com.example.blooddonation;
 
 import androidx.appcompat.app.AppCompatActivity;
+import androidx.appcompat.widget.Toolbar;
 
+import android.content.Intent;
 import android.os.Bundle;
+import android.view.Menu;
+import android.view.MenuInflater;
+import android.view.MenuItem;
 
 public class Activity_UpdateProfile extends AppCompatActivity {
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
-        setContentView(R.layout.activity_update_profile);
+        setContentView(R.layout.activity_update_profile_scrollable);
+        Toolbar toolbar =findViewById(R.id.NonHomeActivity_Toolbar);
+        setSupportActionBar(toolbar);
+        getSupportActionBar().setDisplayHomeAsUpEnabled(true);
+        getSupportActionBar().setTitle("Update Profile");
+
+    }
+    @Override
+    public boolean onCreateOptionsMenu(Menu menu) {
+        MenuInflater inflater = getMenuInflater();
+        inflater.inflate(R.menu.menu_item_for_non_home_activity_toolbar, menu);
+        return true;
+    }
+    @Override
+    public boolean onOptionsItemSelected(MenuItem item) {
+        int id=item.getItemId();
+        if(id==R.id.NonHomeActivity_MenuItem_SearchBlood)
+        {
+            Intent intent=new Intent(this,Activity_SearchBlood.class);
+            startActivity(intent);
+        }
+        return super.onOptionsItemSelected(item);
     }
 }
