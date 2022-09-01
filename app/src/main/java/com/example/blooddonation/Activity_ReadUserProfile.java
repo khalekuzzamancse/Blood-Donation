86c72a289873d05bd0d7292a82eb595b87bb03ff
Khalekuzzaman <khalekuzzamancse@gmail.com>
2022-09-01 12:49:55 +0600

    Updated

--- app/src/main/java/com/example/blooddonation/Activity_ReadUserProfile.java
+++ app/src/main/java/com/example/blooddonation/Activity_ReadUserProfile.java
@@ -4,10 +4,14 @@ import static android.content.ContentValues.TAG;
 
 import androidx.annotation.NonNull;
 import androidx.appcompat.app.AppCompatActivity;
+import androidx.appcompat.widget.Toolbar;
 
 import android.content.Intent;
 import android.os.Bundle;
 import android.util.Log;
+import android.view.Menu;
+import android.view.MenuInflater;
+import android.view.MenuItem;
 import android.view.View;
 import android.widget.AdapterView;
 import android.widget.ArrayAdapter;
@@ -30,7 +34,30 @@ public class Activity_ReadUserProfile extends AppCompatActivity {
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
-        setContentView(R.layout.activity_read_user_profile);
+        setContentView(R.layout.activity_show_profile_scrollable);
 
+        Toolbar toolbar =findViewById(R.id.NonHomeActivity_Toolbar);
+        setSupportActionBar(toolbar);
+        getSupportActionBar().setDisplayHomeAsUpEnabled(true);
+        getSupportActionBar().setTitle("Profile");
+
+    }
+
+    public boolean onCreateOptionsMenu(Menu menu) {
+        MenuInflater inflater = getMenuInflater();
+        inflater.inflate(R.menu.menu_item_for_non_home_activity_toolbar, menu);
+        return true;
+
+
+    }
+    @Override
+    public boolean onOptionsItemSelected(MenuItem item) {
+        int id=item.getItemId();
+        if(id==R.id.NonHomeActivity_MenuItem_SearchBlood)
+        {
+            Intent intent=new Intent(this,Activity_SearchBlood.class);
+            startActivity(intent);
+        }
+        return super.onOptionsItemSelected(item);
     }
 }
