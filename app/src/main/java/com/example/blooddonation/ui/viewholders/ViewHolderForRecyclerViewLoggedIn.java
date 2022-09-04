8b3b06e3888c704060a0cb9424a15b41faa050c6
Khalekuzzaman <khalekuzzamancse@gmail.com>
2022-09-04 17:14:52 +0600

    Updated

--- app/src/main/java/com/example/blooddonation/ui/viewholders/ViewHolderForRecyclerViewLoggedIn.java
+++ app/src/main/java/com/example/blooddonation/ui/viewholders/ViewHolderForRecyclerViewLoggedIn.java
@@ -12,26 +12,26 @@ import androidx.recyclerview.widget.RecyclerView;
 import com.example.blooddonation.R;
 
 public class ViewHolderForRecyclerViewLoggedIn extends RecyclerView.ViewHolder {
-public TextView TextView_ViewHolder_Name;
-      public TextView TextView_ViewHolder_PhoneNumber;
-        public TextView TextView_ViewHolder_Email;
-        public TextView TextView_ViewHolder_BloodGroup;
-        public TextView TextView_ViewHolder_District;
-        public TextView TextView_ViewHolder_SubDistrict;
-        public ImageButton sendCall;
+    public TextView TextView_ViewHolder_Name;
+    public TextView TextView_ViewHolder_PhoneNumber;
+    public TextView TextView_ViewHolder_Email;
+    public TextView TextView_ViewHolder_BloodGroup;
+    public TextView TextView_ViewHolder_District;
+    public TextView TextView_ViewHolder_SubDistrict;
+    public ImageButton sendCall;
     public ImageButton sendEmail;
 
-        public  ViewHolderForRecyclerViewLoggedIn(@NonNull View itemView) {
-            super(itemView);
+    public ViewHolderForRecyclerViewLoggedIn(@NonNull View itemView) {
+        super(itemView);
 
-            TextView_ViewHolder_Name=itemView.findViewById(R.id.Label_RecyclerViewLayout_SearchOnLoggedIn_Name);
-            TextView_ViewHolder_PhoneNumber=itemView.findViewById(R.id.Label_RecyclerViewLayout_SearchOnLoggedIn_PhoneNumber);
-            TextView_ViewHolder_Email=itemView.findViewById(R.id.Label_RecyclerViewLayout_SearchOnLoggedIn_Email);
-            TextView_ViewHolder_District=itemView.findViewById(R.id.Label_RecyclerViewLayout_SearchOnLoggedIn_Location);
-            sendCall=itemView.findViewById(R.id.CardViewButton_call);
-            sendEmail=itemView.findViewById(R.id.CardsendEmail);
+        TextView_ViewHolder_Name = itemView.findViewById(R.id.Label_RecyclerViewLayout_SearchOnLoggedIn_Name);
+        TextView_ViewHolder_PhoneNumber = itemView.findViewById(R.id.Label_RecyclerViewLayout_SearchOnLoggedIn_PhoneNumber);
+        TextView_ViewHolder_Email = itemView.findViewById(R.id.Label_RecyclerViewLayout_SearchOnLoggedIn_Email);
+        TextView_ViewHolder_District = itemView.findViewById(R.id.Label_RecyclerViewLayout_SearchOnLoggedIn_Location);
+        sendCall = itemView.findViewById(R.id.CardViewButton_call);
+        sendEmail = itemView.findViewById(R.id.CardsendEmail);
 
-        }
+    }
 
 
 }
