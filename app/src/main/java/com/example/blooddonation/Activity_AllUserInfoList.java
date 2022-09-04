c5a68cc9a1af1ea33b6dcadbd15dc95838069b55
Khalekuzzaman <khalekuzzamancse@gmail.com>
2022-09-04 20:01:51 +0600

    Updated

--- app/src/main/java/com/example/blooddonation/Activity_AllUserInfoList.java
+++ app/src/main/java/com/example/blooddonation/Activity_AllUserInfoList.java
@@ -1,24 +1,115 @@
 package com.example.blooddonation;
 
 import androidx.appcompat.app.AppCompatActivity;
+import androidx.appcompat.widget.Toolbar;
+import androidx.recyclerview.widget.LinearLayoutManager;
+import androidx.recyclerview.widget.RecyclerView;
 
 import android.os.Bundle;
 import android.util.Log;
+import android.view.Menu;
+import android.view.MenuInflater;
+import android.view.MenuItem;
 
-import com.example.blooddonation.ui.viewholders.AllUserInfoListActivity_ViewHolder;
+import com.example.blooddonation.ui.adapters.AdapterForRecyclerViewLoggedIn;
+import com.example.blooddonation.ui.adapters.AllUserInfoListActivity_Adapter;
+import com.example.blooddonation.ui.datatypes.AllUserInfoListActivity_DataType;
 import com.example.blooddonation.ui.viewmodel.ViewModel_SearchingBlood;
 
+import java.util.ArrayList;
 import java.util.HashMap;
+import java.util.List;
 
 public class Activity_AllUserInfoList extends AppCompatActivity {
-    HashMap<String, HashMap<String, String>> AllUserList = new HashMap<>();
+   private List<AllUserInfoListActivity_DataType>list;
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_all_user_info_list);
-        AllUserList= ViewModel_SearchingBlood.UserInfoListByEmail;
-        Log.i("ALLUser", String.valueOf(AllUserList));
+
+        Toolbar toolbar =findViewById(R.id.NonHomeActivity_Toolbar);
+        setSupportActionBar(toolbar);
+        getSupportActionBar().setDisplayHomeAsUpEnabled(true);
+        getSupportActionBar().setTitle("2 Us");
+
+        list=new ArrayList<>();
+        getUserList();
+        Log.i("OKAY","Alhamdulliah,Done");
+
+        AllUserInfoListActivity_Adapter adapter=new AllUserInfoListActivity_Adapter(Activity_AllUserInfoList.this,list);
+        RecyclerView r=findViewById(R.id.RecyclerView_ActivityAllUserList);
+        r.setLayoutManager(new LinearLayoutManager(Activity_AllUserInfoList.this));
+        r.setAdapter(adapter);
+
+
 
 //
     }
+    @Override
+    public boolean onCreateOptionsMenu(Menu menu) {
+        MenuInflater inflater = getMenuInflater();
+        inflater.inflate(R.menu.menu_item_for_non_home_activity_toolbar, menu);
+        return true;
+    }
+    @Override
+    public boolean onOptionsItemSelected(MenuItem item) {
+        int id=item.getItemId();
+        return super.onOptionsItemSelected(item);
+    }
+    private void getUserList()
+    {
+
+        for (String key1 : ViewModel_SearchingBlood.UserInfoListByEmail.keySet()) {
+            AllUserInfoListActivity_DataType data=new AllUserInfoListActivity_DataType();
+            HashMap<String,String> innerMap = ViewModel_SearchingBlood.UserInfoListByEmail.get(key1);
+            //   Log.i("OKAy",innerMap.get("Name")+"_>"+innerMap.get("Email"));
+            String name=innerMap.get("Name");
+            if(name!=null)
+                data.Name=name;
+            else
+                data.Name="Unknown";
+            String email=innerMap.get("Email");
+            if(email!=null)
+                data.Email=email;
+            else
+                data.Email="Unknown";
+            String phone=innerMap.get("PhoneNumber");
+            if(phone!=null)
+                data.PhoneNumber=phone;
+            else
+                data.PhoneNumber="Unknown";
+            String gender=innerMap.get("Gender");
+            if(gender!=null)
+                data.Gender=gender;
+            else
+                data.Gender="Unknown";
+            String bloodGroup=innerMap.get("BloodGroup");
+            if(bloodGroup!=null)
+                data.BloodGroup=bloodGroup;
+            else
+                data.BloodGroup="Unknown";
+            String dis=innerMap.get("District");
+            if(dis!=null)
+                data.District=dis;
+            else
+                data.District="Unknown";
+            String subDis=innerMap.get("SubDistrict");
+            if(subDis!=null)
+                data.SubDistrict=subDis;
+            else
+                data.SubDistrict="Unknown";
+            ///
+            HashMap<String, String>tmp= MainActivity.model.getSignUserInfo().getValue();
+            String CurrentUserEmail = tmp.get("Email");
+            if (CurrentUserEmail.equals("null")) {
+                data.Email="Available";
+                data.PhoneNumber="Available";
+            }
+            //Log.i("OKAY",data.Name+"->"+data.Email+"->"+data.PhoneNumber+"->"+data.Gender+"->"+data.BloodGroup+"->"+data.District+"->"+data.SubDistrict);
+            list.add(data);
+        }
+
+
+    }
+
 }
--- app/src/main/java/com/example/blooddonation/ui/adapters/AllUserInfoListActivity_Adapter.java
+++ app/src/main/java/com/example/blooddonation/ui/adapters/AllUserInfoListActivity_Adapter.java
@@ -1,6 +1,8 @@
 package com.example.blooddonation.ui.adapters;
 
 import android.content.Context;
+import android.content.Intent;
+import android.net.Uri;
 import android.view.LayoutInflater;
 import android.view.View;
 import android.view.ViewGroup;
@@ -8,11 +10,13 @@ import android.view.ViewGroup;
 import androidx.annotation.NonNull;
 import androidx.recyclerview.widget.RecyclerView;
 
+import com.example.blooddonation.MainActivity;
 import com.example.blooddonation.R;
 import com.example.blooddonation.ui.datatypes.AllUserInfoListActivity_DataType;
 import com.example.blooddonation.ui.datatypes.DataTypeForRecyclerView_ActivitySearchResult;
 import com.example.blooddonation.ui.viewholders.AllUserInfoListActivity_ViewHolder;
 import com.example.blooddonation.ui.viewholders.ViewHolderForRecyclerViewLoggedIn;
+import com.google.android.material.snackbar.Snackbar;
 
 import java.util.List;
 
@@ -44,9 +48,50 @@ public class AllUserInfoListActivity_Adapter extends RecyclerView.Adapter<AllUse
         holder.TV_AllUserInfoListActivity_VH_Phone.setText(phoneNumber);
         String email = list.get(position).Email;
         holder.TV_AllUserInfoListActivity_VH_Email.setText(email);
+        String gender = list.get(position).Gender;
+        holder.TV_AllUserInfoListActivity_VH_Gender.setText(gender);
+        String bloodGroup = list.get(position).BloodGroup;
+        holder.TV_AllUserInfoListActivity_VH_Blood_Group.setText(bloodGroup);
         String dis = list.get(position).District;
         String subDis = list.get(position).SubDistrict;
         holder.TV_AllUserInfoListActivity_VH_Location.setText(dis + "," + subDis);
+
+        ///
+        holder.TV_AllUserInfoListActivity_VH_sendCall.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View view) {
+                String email = MainActivity.model.getSignUserInfo().getValue().get("Email");
+                if (email.equals("null")) {
+
+                    Snackbar.make(holder.TV_AllUserInfoListActivity_VH_sendCall, "Login,Please", Snackbar.LENGTH_SHORT).show();
+                } else {
+                    String number = holder.TV_AllUserInfoListActivity_VH_Phone.getText().toString().trim();
+                    Intent intent = new Intent(Intent.ACTION_DIAL);
+                    intent.setData(Uri.parse("tel:" + number));
+                    context.startActivity(intent);
+                }
+
+
+            }
+        });
+        holder.TV_AllUserInfoListActivity_VH_sendEmail.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View view) {
+                String email = MainActivity.model.getSignUserInfo().getValue().get("Email");
+                if (email.equals("null")) {
+
+                    Snackbar.make(holder.TV_AllUserInfoListActivity_VH_sendEmail, "Login,Please", Snackbar.LENGTH_SHORT).show();
+                } else {
+                    String Email = holder.TV_AllUserInfoListActivity_VH_Email.getText().toString().trim();
+                    Intent emailIntent = new Intent(Intent.ACTION_SENDTO, Uri.fromParts(
+                            "mailto", email, null));
+                    emailIntent.putExtra(Intent.EXTRA_SUBJECT, "This is my subject text");
+                    context.startActivity(Intent.createChooser(emailIntent, null));
+                }
+
+
+            }
+        });
 
 
     }
