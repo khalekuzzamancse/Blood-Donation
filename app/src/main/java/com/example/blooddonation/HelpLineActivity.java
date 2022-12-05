183e3e349aa75096541be9af078b0e8b9ee8ade8
Khalekuzzaman <74848657+khalekuzzamancse@users.noreply.github.com>
2022-12-05 20:11:44 +0600

    okkk

---
+++ app/src/main/java/com/example/blooddonation/HelpLineActivity.java
@@ -0,0 +1,14 @@
+package com.example.blooddonation;
+
+import androidx.appcompat.app.AppCompatActivity;
+
+import android.os.Bundle;
+
+public class HelpLineActivity extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.help_line_activity);
+    }
+}
