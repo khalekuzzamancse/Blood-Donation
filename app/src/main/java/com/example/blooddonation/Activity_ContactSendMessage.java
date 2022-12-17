e46103778913f2abbb667be511e1770116b48f48
Khalekuzzaman <74848657+khalekuzzamancse@users.noreply.github.com>
2022-12-17 19:13:03 +0600

    okay

--- app/src/main/java/com/example/blooddonation/Activity_ContactSendMessage.java
+++ app/src/main/java/com/example/blooddonation/Activity_ContactSendMessage.java
@@ -4,11 +4,16 @@ import androidx.appcompat.app.AppCompatActivity;
 import androidx.appcompat.widget.Toolbar;
 
 import android.content.Intent;
+import android.net.Uri;
 import android.os.Bundle;
 import android.util.Log;
 import android.view.Menu;
 import android.view.MenuInflater;
 import android.view.MenuItem;
+import android.widget.Button;
+import android.widget.EditText;
+import android.widget.ImageButton;
+import android.widget.TextView;
 
 public class Activity_ContactSendMessage extends AppCompatActivity {
 
@@ -20,6 +25,23 @@ public class Activity_ContactSendMessage extends AppCompatActivity {
 //        setSupportActionBar(toolbar);
 //        getSupportActionBar().setDisplayHomeAsUpEnabled(true);
 //        getSupportActionBar().setTitle("Contact Us");
+        Toolbar toolbar =findViewById(R.id.NonHomeActivity_Toolbar);
+        setSupportActionBar(toolbar);
+        getSupportActionBar().setDisplayHomeAsUpEnabled(true);
+        getSupportActionBar().setTitle("Contact Us");
+
+        ImageButton send=findViewById(R.id.imageButton);
+        send.setOnClickListener(view -> {
+            EditText writemsg=findViewById(R.id.writemsg);
+            String msg=writemsg.getText().toString();
+            Intent emailIntent = new Intent(Intent.ACTION_SENDTO, Uri.fromParts(
+                    "mailto", "jahidrana2020@gmail.com", null));
+            emailIntent.putExtra(Intent.EXTRA_SUBJECT, "This is my subject text");
+            startActivity(Intent.createChooser(emailIntent, null));
+        });
+
+
+
 
     }
 //    @Override
@@ -34,5 +56,7 @@ public class Activity_ContactSendMessage extends AppCompatActivity {
 //
 //        return super.onOptionsItemSelected(item);
 //    }
+
+
 
 }
